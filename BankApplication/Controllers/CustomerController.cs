7d1d79b734cabaed4fb6e1b5da4c32222370bd05
sara <sara.berling@hotmail.com>
2019-05-14 21:20:27 +0200

    add Controllers, Models, Views, Migrations and Data. Set up EF and identity plus NewCustomer and SearchCustomer

---
+++ BankApplication/Controllers/CustomerController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BankApplication.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BankApplication.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private BankAppDataContext _context;
+
+        public CustomerController(BankAppDataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NewCustomer()
+        {
+            var model = new Customers();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult NewCustomer(Customers values)
+        {
+            //if (ModelState.IsValid)
+            //{
+            values.Gender = "female"; //Fixa radiobuttons sen!
+            _context.Customers.Add(values);
+            _context.SaveChanges();
+            ModelState.Clear();
+            //}
+
+            return View("Index");
+        }
+
+        public IActionResult SearchCustomer()
+        {
+            var model = new Customers();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SearchCustomer(int mySearch)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.CustomerId == mySearch);
+
+            return View("Visa", customer);
+        }
+    }
+}
--- BankApplication/Startup.cs
+++ BankApplication/Startup.cs
@@ -6,15 +6,33 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using BankApplication.Models;
+using BankApplication.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace BankApplication
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer
+            (Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +43,15 @@ namespace BankApplication
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseAuthentication();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                name: "Default",
+                template: "{controller=Customer}/{action=NewCustomer}"
+                    );
             });
         }
     }
